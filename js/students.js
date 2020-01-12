fa53c43e3a8a29cbb9bf243becc158a836df6b70
Nyilasy Péter <nyilasypeter@gmail.com>
2020-01-12 10:18:56 +0100

    first commit

---
+++ js/students.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('submitBtn').addEventListener('click', function(event){
+        event.preventDefault();
+        $("#myModal").modal('hide')
+    })
+});
