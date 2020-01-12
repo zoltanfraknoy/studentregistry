d950b0252bc866e6aa1d01ab6640fe5c8704a11a
Nyilasy Péter <nyilasypeter@gmail.com>
2020-01-12 10:26:39 +0100

    bootstrap from local

--- js/students.js
+++ js/students.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('submitBtn').addEventListener('click', function(event){
         event.preventDefault();
-        $("#myModal").modal('hide')
-    })
+        $("#myModal").modal('hide');
+    });
 });
