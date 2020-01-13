adb6bd37db4aae06062d8e96fbc3448cb4ea3fb7
Nyilasy Péter <nyilasypeter@gmail.com>
2020-01-13 10:29:50 +0100

    modal show added

--- js/students.js
+++ js/students.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('addStudent').addEventListener('click', function (event) {
+        $("#myModal").modal();
+    });
+
     document.getElementById('submitBtn').addEventListener('click', function(event){
         event.preventDefault();
         $("#myModal").modal('hide');
