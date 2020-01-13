c521c8c4f017b2bd9f688640eac87f8543a3e7cd
Zoltán <zoltanfraknoy@gmail.com>
2020-01-13 22:51:11 +0100

    new user addition created

--- js/students.js
+++ js/students.js
@@ -8,7 +8,30 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.getElementById('submitBtn').addEventListener('click', function (event) {
-        event.preventDefault();
+
+        if (document.querySelector('#surname').value === ''
+            || document.querySelector('#cname').value === ''
+            || document.querySelector('#year').value === ''
+            || document.querySelector('#mail').value === '') {
+
+            event.preventDefault();
+        }
+
+        let newTr = document.querySelectorAll('tr')[1].cloneNode(true);
+        //document.createElement('tr');
+        let table = document.querySelector('tbody');
+        let newRow = table.appendChild(newTr);
+
+        let surName = document.querySelectorAll('tr')[document.querySelectorAll('tr').length - 1].cells[0];
+        let cName = document.querySelectorAll('tr')[document.querySelectorAll('tr').length - 1].cells[1];
+        let yearInSchool = document.querySelectorAll('tr')[document.querySelectorAll('tr').length - 1].cells[2];
+        let mail = document.querySelectorAll('tr')[document.querySelectorAll('tr').length - 1].cells[3];
+
+        surName.innerText = document.querySelector('#surname').value;
+        cName.innerText = document.querySelector('#cname').value;
+        yearInSchool.innerText = document.querySelector('#year').value;
+        mail.innerText = document.querySelector('#mail').value;
+
         $("#myModal").modal('hide');
     });
 
@@ -38,3 +61,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 });
+
+//Az elk**t idő emlékére
+
+//ez működik
+/* let surName = newRow.insertCell(0);
+ let cName = newRow.insertCell(1);
+ let yearInSchool = newRow.insertCell(2);
+ let mail = newRow.insertCell(3);
+ let buttons = newRow.insertCell(4);
+ buttons.className = 'text-right';*/
+
+/*let delButton = document.createElement('input');
+delButton.type = "button";
+delButton.className = "btn btn-info btn-sm delete";
+delButton.id = 'delete';
+delButton.appendChild(buttonSpanDel);
+buttonSpanDel.innerHTML = 'Törlés';*/
+
+// let editButton = document.querySelector('.edit').cloneNode(true);
+// let delButton = document.querySelector('.delete').cloneNode(true);
+
+
+// buttons.appendChild(editButton);
+//buttons.appendChild(delButton);
+
+//felejtős
+/*let buttonSpanDel = document.createElement('span');
+buttonSpanDel.className = 'glyphicon glyphicon-pencil';
+let buttonSpanEdit = document.createElement('span');
+buttonSpanEdit.className = 'glyphicon glyphicon-pencil';*/
+
+//szintén felejtős
+/*let editButton = document.createElement('input');
+editButton.type = "button";
+editButton.className = "btn btn-info btn-sm edit";
+editButton.id = 'edit';
+editButton.appendChild(buttonSpanEdit);
+buttonSpanEdit.innerHTML = "Szerkeszt";*/
